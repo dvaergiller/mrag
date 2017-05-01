c1986ad1ceb80ec5d9d1429f6b655a2df6be190a
Mattias Jansson <mattias.jansson@kivra.com>
2017-05-01 16:22:27 +0200

    Without locks

--- Mrag.hs
+++ Mrag.hs
@@ -5,25 +5,22 @@ import Network.Wai.Handler.Warp
 import System.Directory
 import System.Environment
 import System.FilePath
-import qualified Control.Concurrent.Lock as L
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Text as T
 
 main = do
   [port, docRoot] <- getArgs
-  lock <- L.new
-  run (read port) $ waiApp lock docRoot
+  run (read port) $ waiApp docRoot
 
-waiApp lock docRoot request respond = response >>= respond
+waiApp docRoot request respond = response >>= respond
   where path = joinPath $ docRoot : map T.unpack (pathInfo request)
         response =
-          case requestMethod request of
-          "POST" -> strictRequestBody request >>= doPost lock path
-          _ -> return $ responseLBS status405 [] "Method not Allowed"
+          if requestMethod request == "POST"
+          then strictRequestBody request >>= doPost path
+          else return $ responseLBS status405 [] "Method not Allowed"
 
-doPost lock path content =
-  L.with lock appendLine >> return (responseLBS status200 [] "")
-  where appendLine = do
-          createDirectoryIfMissing True $ takeDirectory path
-          BS.appendFile path content
-          BS.appendFile path "\n"
+doPost path content = do
+  createDirectoryIfMissing True $ takeDirectory path
+  BS.appendFile path content
+  BS.appendFile path "\n"
+  return $ responseLBS status200 [] ""
